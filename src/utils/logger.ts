b87a45f0ccb578e9445ecb322c654c4658cb0d38
Sarthak Kavidayal <sarthak.kavidayal@thewitslab.com>
2024-09-18 16:46:46 +0530

    added grafana

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,10 +1,13 @@
-import winston from "winston";
+import winston, { format, transports } from "winston";
+import LokiTransport from "winston-loki";
 require("winston-daily-rotate-file");
 
 // const config = require('../config.json');
-// let environment = process.env.NODE_ENV || 'development';
+let envLocation = process.env.NODE_ENV || "development";
 let environment = process.env.env || "development";
 environment = environment.trim();
+
+console.log("_____________", process.env.LOKI_HOST, envLocation);
 
 export const logger = winston.createLogger({
   level: process.env.LOG_LEVEL || "info",
@@ -16,16 +19,17 @@ export const logger = winston.createLogger({
   ),
   defaultMeta: { service: process.env.app_name },
   transports: [
-    // - Write to all logs with level `info` and below to `combined.log`
-    // - Write all logs error (and below) to `error.log`.
-    new winston.transports.File({ filename: "Logs/error.log", level: "error" }),
-    new winston.transports.File({ filename: "Logs/combined.log" }),
-    // new winston.transports.DailyRotateFile({
-    //     filename: process.env.app_name + '-%DATE%.log',
-    //     dirname: 'Logs/',
-    //     datePattern: 'YYYY-MM-DD',
-    //     maxFiles: '1d'
-    // })
+    new LokiTransport({
+      host: process.env.LOKI_HOST as string,
+      labels: { app: process.env.LOKI_APP_NAME || `infra_${envLocation}` },
+      json: true,
+      format: format.json(),
+      replaceTimestamp: true,
+      onConnectionError: (err: any) => logger.error(err),
+    }),
+    new transports.Console({
+      format: format.combine(format.simple(), format.colorize()),
+    }),
   ],
 });
 
@@ -33,15 +37,15 @@ export const logger = winston.createLogger({
 // If we're not in production then log to the `console` with the format:
 // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
 //
-if (process.env.env !== "production") {
-  logger.add(
-    new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.timestamp({
-          format: "YYYY-MM-DD HH:mm:ss",
-        }),
-        winston.format.simple()
-      ),
-    })
-  );
-}
+// if (process.env.env !== "production") {
+//   logger.add(
+//     new winston.transports.Console({
+//       format: winston.format.combine(
+//         winston.format.timestamp({
+//           format: "YYYY-MM-DD HH:mm:ss",
+//         }),
+//         winston.format.simple()
+//       ),
+//     })
+//   );
+// }
